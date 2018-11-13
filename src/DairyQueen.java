5c4cbef3b375d14679d9236706a618a6dde5cd74
MattSchwartzkopf <schwartzkopfm@gmail.com>
2018-11-13 16:44:37 -0500

    Adding files

---
+++ src/DairyQueen.java
@@ -0,0 +1,10 @@
+
+public class DairyQueen extends IceCreamShop {
+
+	@Override
+	public IceCream getIceCream(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/NonRecursive.java
@@ -0,0 +1,10 @@
+
+public class NonRecursive extends SortFactory {
+
+	@Override
+	public SortingAlgorithm getAlgorithm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
